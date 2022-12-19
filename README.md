60d0bfc3445c22c236691731dcdbf5aff1f34418
Fuzz <gitlab@dazraf.io>
2022-12-19 02:07:08 +0000

    typical hardhat tasks

--- README.md
+++ README.md
@@ -19,15 +19,103 @@ Then run the following to install all dependencies.
 npm install
 ```
 
-## Build
+Install the Hardhat shortcut:
+
+```
+npm install --global hardhat-shorthand
+```
+
+Set the `TOKEN_CONTRACT` environment variable either in the `.env` file:
+
+```.env
+TOKENB_CONTRACT=[token contract address]
+```
+
+or within your shell:
+
+```
+export TOKEN_CONTRQACT=[token contract address]
+```
+
+## Hardhat Tasks
+
+## A note about task execution
+
+When executing a task, if you want to target the xinfin network, Apothem or mainnet, you will need to set the network in your `GLOBAL OPTIONS` section:
+
+```
+--network xinfin
+```
+
+### Compilation
 
 ```bash
-npm run compile
+hh compile
 ```
 
-## Deploy
+### Deployment
 
 ```
-npm run deploy
+hh [GLOBAL OPTIONS] deploy
+```
+
+> **Please Note!**
+>
+> The Hardhat configuration uses the environment variable `TOKEN_CONTRACT` to reference the deployed Crescite contract.
+>
+> You can either set this with
+>
+> ```
+> export TOKEN_CONTRACT=[token contract address]
+> ```
+> 
+> Or as part of the deployment (e.g. when working with the Apothem test network)
+>
+> ```
+> export TOKEN_CONTRACT=$(hh --network xinfin deploy)
+> ```
+>
+
+### Print Config 
+
+```
+Usage: hardhat [GLOBAL OPTIONS] print-config
+
+print-config: Prints the config
+```
+
+### Total Supply 
+
+```
+Usage: hardhat [GLOBAL OPTIONS] total-supply
+
+total-supply: Prints out the total supply of the token
+```
+
+
+### Has Role
+
+```
+Usage: hardhat [GLOBAL OPTIONS] has-role --account <STRING> --role <STRING>
+
+OPTIONS:
+
+  --account     XDC account  
+  --role        one of the supported roles: DEFAULT_ADMIN_ROLE, SNAPSHOT_ROLE, PAUSER_ROLE, MINTER_ROLE 
+
+has-role: Determine if an acount has a role
+```
+
+### Mint
+
+```
+Usage: hardhat [GLOBAL OPTIONS] mint --account <STRING> --amount <STRING>
+
+OPTIONS:
+
+  --account     the address of the account in 0x... form 
+  --amount      the amount of tokens being minted to the address 
+
+mint: Mint tokens
 ```
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 require("dotenv").config({ path: ".env" });
+import { Crescite } from './typechain-types/contracts/Crescite';
+import { BigNumberish } from "ethers";
 
-const XINFIN_NETWORK_URL = process.env.XINFIN_NETWORK_URL;
+const XINFIN_NETWORK_URL = process.env.XINFIN_NETWORK_URL as string;
 const XINFIN_PRIVATE_KEY = process.env.XINFIN_PRIVATE_KEY as string;
+const TOKEN_CONTRACT = process.env.TOKEN_CONTRACT as string;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -24,8 +28,97 @@ const config: HardhatUserConfig = {
   },
 };
 
-console.log("config:")
-console.log(JSON.stringify(config));
-console.log();
-
 export default config;
+
+task("print-config", "Prints the config", async (taskArgs, hre) => {
+  console.log("config:")
+  console.log(JSON.stringify(config));
+  console.log();
+});
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("deploy", "Deploys the contract", async (taskArgs, hre) => {
+  const Crescite = await hre.ethers.getContractFactory("Crescite");
+  const crescite = await Crescite.deploy();
+  await crescite.deployed();
+  console.log(crescite.address);
+});
+
+task("total-supply", "Prints out the total supply of the token", async (taskArgs, hre) => {
+  const crescite = await bindToCrescite(hre);
+  console.log(getTotalSupply(crescite, hre));
+})
+
+task("mint", "Mint tokens")
+.addParam("account", "the address of the account in 0x... form")
+.addParam("amount", "the amount of tokens being minted to the address")
+.setAction(async ({account, amount}, hre) => {
+  const crescite = await bindToCrescite(hre);
+  console.log(`Minting ${amount} tokens to account ${account}`);
+  const amountWei = hre.ethers.utils.parseEther(amount);
+  console.log("total supply before minting: ", await getTotalSupply(crescite, hre));
+  const tx = await crescite.mint(xdcAddressToEth(account), amountWei);
+  const receipt = await tx.wait();
+  console.log('gas used:', formatEther(receipt.gasUsed, hre));
+  console.log("total supply after minting: ", await getTotalSupply(crescite, hre));
+});
+
+task("has-role", "Determine if an acount has a role")
+.addParam("role", "one of the supported roles: DEFAULT_ADMIN_ROLE, SNAPSHOT_ROLE, PAUSER_ROLE, MINTER_ROLE")
+.addParam("account", "XDC account ")
+.setAction(async ({role, account}, hre) => {
+  const crescite = await bindToCrescite(hre);
+  const address = xdcAddressToEth(account);
+  const roleBinary = hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes('MINTER_ROLE'));
+  console.log(roleBinary);
+  const hasRole = await crescite.hasRole(roleBinary, address);
+  if (hasRole) {
+    console.log(`account ${account} has the role ${role}`);
+  } else {
+    console.log(`account ${account} does not have the role ${role}`);
+  }
+});
+
+// -- Utility functions
+
+async function bindToCrescite(hre: HardhatRuntimeEnvironment): Promise<import("/Users/fuzz/dev/lab577/crescite-token/typechain-types/index").Crescite> {
+  const CresciteFactory = await hre.ethers.getContractFactory("Crescite");
+  const crescite = CresciteFactory.attach(TOKEN_CONTRACT);
+  return crescite;
+}
+
+async function getTotalSupply(crescite: Crescite, hre: HardhatRuntimeEnvironment): Promise<string> {
+  const totalSupply = await crescite.totalSupply()
+  return formatEther(totalSupply, hre);
+}
+
+function formatEther(amount: BigNumberish, hre: HardhatRuntimeEnvironment): string {
+  return hre.ethers.utils.formatEther(amount);
+}
+
+function xdcAddressToEth(address: string): string {
+  if (address.startsWith('0x')) {
+    return address;
+  }
+  if (address.startsWith('xdc')) {
+    return '0x' + address.slice(3);
+  }
+  throw new Error(`unknown address type ${address}`);
+}
+
+function ethAddressToXdc(address: string): string {
+  if (address.startsWith('xdc')) {
+    return address;
+  }
+  if (address.startsWith('0x')) {
+    return 'xdc' + address.slice(2);
+  }
+  throw new Error(`unknown address type: ${address}`);
+}
