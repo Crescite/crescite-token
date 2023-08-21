7c4a00ec3e8b7aed1d6669f5fb628695dcff04d9
John Grimsey <jg@lab577.io>
2023-08-21 16:23:55 +0100

    test: add first test of Crescite contract

---
+++ test/Crescite.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { Crescite } from '../typechain-types';
+
+describe('Crescite', () => {
+  it('should not mint any tokens to owner account', async () => {
+    const [owner] = await ethers.getSigners();
+
+    const cresciteToken = await ethers.deployContract("Crescite") as Crescite;
+
+    const ownerBalance = await cresciteToken.balanceOf(owner.address);
+    expect(ownerBalance).to.equal(0);
+  })
+})
