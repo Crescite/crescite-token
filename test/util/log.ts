28b45d5573a3a2255fca52c4f488bc4ddf582f5f
John Grimsey <jg@lab577.io>
2023-08-21 16:23:56 +0100

    chore: remove unused code, adds comment

--- test/util/log.ts
+++ test/util/log.ts
@@ -1,7 +1,14 @@
-require("dotenv").config({ path: ".env" });
+require('dotenv').config({ path: '.env' });
 
+/**
+ * if .env file contains DEBUG_LOGGING_IN_TESTS=true then log will be printed
+ *
+ * For use inside unit test files.
+ *
+ * @param args Same parameters as console.log
+ */
 export function log(...args: any) {
-  if(['true', 1, 'yes', 'on'].includes(process.env.DEBUG_LOGGING_IN_TESTS as any)) {
+  if (['true', 1, 'yes', 'on'].includes(process.env.DEBUG_LOGGING_IN_TESTS as any)) {
     console.log(...args);
   }
 }
