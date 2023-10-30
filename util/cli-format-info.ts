2e6b67aaadd81093b351af4bb09e7c36c3a8078c
John Grimsey <jg@lab577.io>
2023-10-30 19:33:44 +0000

    chore: utility functions for info/success cli output

---
+++ util/cli-format-info.ts
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+export const cliFormatInfo = (msg: string) => chalk.yellow(msg);
---
+++ util/cli-format-success.ts
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+export const cliFormatSuccess = (msg: string) => chalk.bold(chalk.green(msg));
