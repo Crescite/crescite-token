b0a48bedcb595b38e76ed596de8c2299b216e0b8
Mathieu Gauthron <mathieu@matlux.net>
2023-12-20 00:43:57 +0000

    feat: added new tests to commoditiesBroker contract

--- contracts/CommodityBroker.sol
+++ contracts/CommodityBroker.sol
@@ -20,14 +20,21 @@ contract CommodityBroker is DSMath, Ownable, AccessControl {
     address private treasuryWalletAddress;
     address private xaucERC20TokenAddress;
     address private usdcERC20TokenAddress;
+    ERC20 private usdcERC20;
+    ERC20 private xaucERC20;
 
     constructor(address _treasuryWalletAddress,
       address _xaucERC20TokenAddress,
-      address _usdcERC20TokenAddress) {
+      address _usdcERC20TokenAddress,
+      uint256 newRate, uint256 newSpread) {
         _grantRole(ADMIN_ROLE, msg.sender);
         treasuryWalletAddress = _treasuryWalletAddress;
         xaucERC20TokenAddress = _xaucERC20TokenAddress;
         usdcERC20TokenAddress = _usdcERC20TokenAddress;
+        xaucERC20 = ERC20(xaucERC20TokenAddress);
+        usdcERC20 = ERC20(usdcERC20TokenAddress);
+        XAUC_USDC_Rate = newRate;
+        spread = newSpread;
     }
 
     // Setter function to set the rate and spread
@@ -75,6 +82,7 @@ contract CommodityBroker is DSMath, Ownable, AccessControl {
         return treasuryWalletAddress;
     }
 
+    // Helper function to convert uint256 to string
     function uintToString(uint256 value) internal pure returns (string memory) {
         // Base case
         if (value == 0) {
@@ -97,38 +105,90 @@ contract CommodityBroker is DSMath, Ownable, AccessControl {
         return string(buffer);
     }
 
+    function addressToString(address _addr) public pure returns(string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory data = abi.encodePacked(_addr);
+        bytes memory str = new bytes(42);
+        str[0] = '0';
+        str[1] = 'x';
+        for (uint256 i = 0; i < 20; i++) {
+            str[2+i*2] = alphabet[uint8(data[i] >> 4)];
+            str[3+i*2] = alphabet[uint8(data[i] & 0x0f)];
+        }
+        return string(str);
+    }
+
+
+    // Function to create a detailed error message with a custom message
+    function createErrorMessage(string memory message, uint256 available, uint256 required)
+      internal pure returns (string memory) {
+        return string(abi.encodePacked(
+            message,
+            ": available=", 
+            uintToString(available),
+            ", required=", 
+            uintToString(required)
+        ));
+    }
+
+    function checkBalance(ERC20 token, address account, uint256 amount, string memory tokenName) private view {
+        uint256 balance = token.balanceOf(account);
+        require(balance >= amount,
+            createErrorMessage(
+                string(abi.encodePacked("Not enough ", tokenName, " in ", addressToString(account)," wallet")),
+                balance, 
+                amount
+            )
+        );
+    }
+
+    function checkAllowance(ERC20 token, 
+                            address owner, 
+                            address spender, 
+                            uint256 amount, 
+                            string memory tokenName) private view {
+        uint256 allowance = token.allowance(owner, spender);
+        require(allowance >= amount,
+            createErrorMessage(
+                string(abi.encodePacked("Not enough allowance of ", 
+                                          tokenName, " in ", 
+                                          addressToString(owner),
+                                          " wallet for spender=", addressToString(spender))),
+                allowance, 
+                amount
+            )
+        );
+    }
 
     function purchase(uint256 xaucAmount, uint256 rate) public {
       // Check if the rate matches the ask rate
       require(rate == getAsk(), "Incorrect rate provided");
 
-      uint256 balanceOfXauc = ERC20(xaucERC20TokenAddress).
-        balanceOf(treasuryWalletAddress);
-      // Check if the treasury has enough XAUC tokens
-      require(balanceOfXauc >= xaucAmount,
-        string(abi.encodePacked("Not enough XAUC in treasury: available=", 
-                            uintToString(balanceOfXauc), 
-                    
-                            ", required=", 
-                            uintToString(xaucAmount))));
+      // check if xaucAmount is greater than 0
+      require(xaucAmount > 0, "Purchase amount must be greater than zero");
 
-      uint256 balanceOfUsdc = ERC20(xaucERC20TokenAddress).
-        balanceOf(treasuryWalletAddress);
+      // Check if the treasury has enough XAUC tokens
+      checkBalance(xaucERC20, treasuryWalletAddress, xaucAmount, "XAUC");
+      // Check if the treasury has allowed enough XAUC to be spent
+      checkAllowance(xaucERC20, treasuryWalletAddress, address(this), xaucAmount, "XAUC");
+
       // Transfer USDC from sender to treasury
       // Assuming USDC token address is stored in usdcTokenAddress
       // and that the contract is already approved to spend the sender's USDC
+      // Calculate USDC amount needed
       uint256 usdcAmount = wmul(xaucAmount, rate);
 
-      require(balanceOfUsdc >= usdcAmount,
-        string(abi.encodePacked("Not enough USDC in the customer's wallet: available=", 
-                              uintToString(balanceOfUsdc), 
-                              ", required=", 
-                              uintToString(usdcAmount))));
-      require(ERC20(usdcERC20TokenAddress).transferFrom(msg.sender, treasuryWalletAddress, usdcAmount), "USDC transfer failed");
+      // Check if the customer has enough USDC in their wallet
+      checkBalance(usdcERC20, msg.sender, usdcAmount, "USDC");
+
+      // Check if the customer has allowed enough USDC to be spent
+      checkAllowance(usdcERC20, msg.sender, address(this), usdcAmount, "USDC");
+
+      require(usdcERC20.transferFrom(msg.sender, treasuryWalletAddress, usdcAmount), "USDC transfer failed");
 
       // Transfer XAUC from treasury to sender
       // Assuming the contract has the necessary permissions to transfer treasury's XAUC tokens
-      require(ERC20(xaucERC20TokenAddress).transferFrom(treasuryWalletAddress, msg.sender, xaucAmount), "XAUC transfer failed");
+      require(xaucERC20.transferFrom(treasuryWalletAddress, msg.sender, xaucAmount), "XAUC transfer failed");
     }
 
 
--- test/CommodityBroker.ts
+++ test/CommodityBroker.ts
@@ -4,8 +4,17 @@ import { ethers } from 'hardhat';
 import { CommodityBroker, CommodityToken, USDC_Mock, GoldToken } from '../typechain-types';
 import { big } from './util/big';
 
+  //  admin.address
+  // '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+  // treasuryWallet.address
+  // '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
+  // user.address
+  // '0x35D8DAe5215aA7a53D27a8227C0774e67795f53F'
+
+
 async function deployFixtures() {
   const [admin, treasuryWallet, user ] = await ethers.getSigners();
+
   // ... deployment logic ...
   const goldToken = (await ethers.deployContract('GoldToken', admin)) as GoldToken;
   await goldToken.deployed();
@@ -13,16 +22,17 @@ async function deployFixtures() {
   await usdcToken.deployed();
 
   // Mint a sufficient amount of XAUC to the treasury wallet
-  const mintAmount = big(100_000_000); // Adjust this amount as needed
-  await goldToken.connect(admin).mint(treasuryWallet.address, mintAmount);
+  await goldToken.connect(admin).mint(treasuryWallet.address, big(1_000)); // Adjust this amount as needed
   // Give sufficient USDC to the user wallet
-  await usdcToken.connect(treasuryWallet).mint(user.address, big(100_000_000));
+  await usdcToken.connect(treasuryWallet).mint(user.address, big(64_640));
 
   // Deploy CommodityBroker
   const CommodityBrokerFactory = await ethers.getContractFactory("CommodityBroker");
-  const commodityBroker = (await CommodityBrokerFactory.deploy(treasuryWallet.address, goldToken.address, usdcToken.address)) as CommodityBroker;
+  const commodityBroker = (await CommodityBrokerFactory.deploy(treasuryWallet.address, goldToken.address, usdcToken.address, big(64), big(0.01))) as CommodityBroker;
   // const commodityBroker = (await ethers.deployContract('CommodityBroker', admin)) as CommodityBroker;
   await commodityBroker.deployed();
+  // await commodityBroker.connect(admin).setRateAndSpread(big(64), big(0.01)); // 100 XAUC per USDC, 100 bps spread
+
 
   
   return { admin, user, treasuryWallet, usdcToken, goldToken, commodityBroker };
@@ -35,7 +45,6 @@ describe("CommodityBroker Contract", function () {
 
     expect(goldToken.allowance(treasuryWallet.address, admin.address)).eventually.to.equal(0);
 
-    await commodityBroker.connect(admin).setRateAndSpread(big(64), big(0.01)); // 100 XAUC per USDC, 100 bps spread
     await usdcToken.connect(user).approve(commodityBroker.address, big(10_000));
     await goldToken.connect(treasuryWallet).approve(commodityBroker.address, big(10_000));
 
@@ -47,14 +56,122 @@ describe("CommodityBroker Contract", function () {
     // Check balances
     expect(await goldToken.balanceOf(user.address)).to.equal(0);
     expect(await usdcToken.balanceOf(treasuryWallet.address)).to.equal(0);
+    expect(await goldToken.balanceOf(treasuryWallet.address)).to.equal(big(1000));
+    expect(await usdcToken.balanceOf(user.address)).to.equal(big(64640));
 
     await commodityBroker.connect(user).purchase(xaucAmount, rate);
 
     // Check balances
     expect(await goldToken.balanceOf(user.address)).to.equal(xaucAmount);
     expect(await usdcToken.balanceOf(treasuryWallet.address)).to.equal(big(64.64));
+    expect(await goldToken.balanceOf(treasuryWallet.address)).to.equal(big(999));
+    expect(await usdcToken.balanceOf(user.address)).to.equal(big(64575.36));
 
   });
 
-  // ... Other tests ...
+  it("should revert purchase at incorrect rate", async function () {
+    const { admin, user, treasuryWallet, usdcToken, goldToken, commodityBroker } = await loadFixture(deployFixtures);
+    const xaucAmount = ethers.utils.parseEther("1"); // 1 XAUC
+    const incorrectRate = (await commodityBroker.getAsk()).add(1); // Incorrect rate
+
+    await expect(commodityBroker.connect(user).purchase(xaucAmount, incorrectRate)).to.be.revertedWith("Incorrect rate provided");
+  });
+
+  it("should revert if treasury didn't give enough approval to CommoditiesContract to spend its XAUC", async function () {
+    const { admin, user, treasuryWallet, usdcToken, goldToken, commodityBroker } = await loadFixture(deployFixtures);
+    const xaucAmount = big(1);
+    const rate = await commodityBroker.getAsk();
+
+    await expect(commodityBroker.connect(user).purchase(xaucAmount, rate)).
+            to.be.revertedWith("Not enough allowance of XAUC in " +
+            treasuryWallet.address.toLowerCase() +
+            " wallet for spender=" +
+            commodityBroker.address.toLowerCase() +
+            ": available=0, required=1000000000000000000");
+  });
+
+  it("should revert if treasury does not have enough XAUC", async function () {
+    const { admin, user, treasuryWallet, usdcToken, goldToken, commodityBroker } = await loadFixture(deployFixtures);
+    const xaucAmount = big(1001); // More than treasury has
+    const rate = await commodityBroker.getAsk();
+
+    expect(commodityBroker.connect(user).purchase(xaucAmount, rate)).
+            eventually.rejectedWith("Not enough XAUC in " +
+            treasuryWallet.address.toLowerCase() +
+            " wallet: available=1000000000000000000000, required=1001000000000000000000");
+  });
+  it("should revert if user does not have enough USDC", async function () {
+    const { user, commodityBroker, goldToken, usdcToken, treasuryWallet, admin } = await loadFixture(deployFixtures);
+    await goldToken.connect(admin).mint(treasuryWallet.address, big(3000));
+    const xaucAmount = big(4000);
+    expect(await goldToken.balanceOf(treasuryWallet.address)).to.equal(xaucAmount);
+    expect(await usdcToken.balanceOf(user.address)).to.equal(big(64640));
+
+    const rate = await commodityBroker.getAsk();
+    // Approve an "infinite" amount of XAUC tokens
+    await goldToken.connect(treasuryWallet).approve(commodityBroker.address, ethers.constants.MaxUint256);
+
+
+    // Assuming user has less USDC than required
+    await expect(commodityBroker.connect(user).purchase(xaucAmount, rate))
+        .to.be.revertedWith("Not enough USDC in " +
+            user.address.toLowerCase() +
+            " wallet: available=" +
+            big(64640) +
+            ", required=" +
+            big(64.64 * 4000));
+
+    expect(await goldToken.balanceOf(treasuryWallet.address)).to.equal(xaucAmount);
+    expect(await usdcToken.balanceOf(user.address)).to.equal(big(64640));
+  });
+
+  it("should revert if user has not approved enough USDC", async function () {
+      const { user, usdcToken, commodityBroker, goldToken, treasuryWallet } = await loadFixture(deployFixtures);
+      const xaucAmount = big(1);
+      const rate = await commodityBroker.getAsk();
+      // Approve an "infinite" amount of XAUC tokens
+      await goldToken.connect(treasuryWallet).approve(commodityBroker.address, ethers.constants.MaxUint256);
+
+      // User approves less than required
+      await usdcToken.connect(user).approve(commodityBroker.address, big(63.64));
+
+      const allowance = await usdcToken.allowance(user.address, commodityBroker.address);
+      expect(allowance).to.equal(big(63.64));
+
+      await expect(commodityBroker.connect(user).purchase(xaucAmount, rate))
+          .to.be.revertedWith("Not enough allowance of USDC in " +
+              user.address.toLowerCase() +
+              " wallet for spender=" + 
+              commodityBroker.address.toLowerCase() +
+              ": available=" +
+              big(63.64) +
+              ", required=" +
+              big(64.64)
+              );
+  });
+
+
+  it("should revert setting rate and spread by non-admin", async function () {
+    const { user, commodityBroker } = await loadFixture(deployFixtures);
+    
+    await expect(commodityBroker.connect(user).setRateAndSpread(big(50), big(0.02)))
+        .to.be.revertedWith("Caller is not an admin");
+  });
+
+  it("should revert changing treasury wallet by non-admin", async function () {
+    const { user, commodityBroker } = await loadFixture(deployFixtures);
+    const newTreasuryWallet = user.address; // Example new address
+
+    await expect(commodityBroker.connect(user).setTreasuryWalletAddress(newTreasuryWallet))
+        .to.be.revertedWith("Caller is not an admin");
+  });
+
+  it("should revert purchasing with zero amount", async function () {
+    const { user, commodityBroker } = await loadFixture(deployFixtures);
+    const rate = await commodityBroker.getAsk();
+
+    await expect(commodityBroker.connect(user).purchase(big(0), rate))
+        .to.be.revertedWith("Purchase amount must be greater than zero");
+  });
+
 });
